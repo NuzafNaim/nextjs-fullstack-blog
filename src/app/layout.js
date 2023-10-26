899944b56bb4fac12e5122eb4f10b4a19ad52a84
Nuzaf Naim Saikat <hello@nuzafnaim.dev>
2023-10-26 15:02:52 +0600

    Folder Structure

---
+++ src/app/layout.js
@@ -0,0 +1,27 @@
+import Navbar from "@/components/Navbar/Navbar";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Footer from "@/components/footer/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Nest JS Full Stack Blog App",
+  description: "The best blog app!",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="container">
+          <div className="wrapper">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
